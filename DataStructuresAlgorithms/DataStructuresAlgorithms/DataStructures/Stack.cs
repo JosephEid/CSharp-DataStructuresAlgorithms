690f7b6a564fcd9d1ce8c0ea2403eed6ed93d10b
JosephEid <josephceid@gmail.com>
2022-03-23 17:56:20 +0000

    Implemented Stack

---
+++ DataStructuresAlgorithms/DataStructuresAlgorithms/DataStructures/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace DataStructuresAlgorithms.DataStructures
+{
+    public class Stack
+    {
+        public Stack(int size)
+        {
+            Array = new int[size];
+            Top = -1;
+            Max = size;
+        }
+
+        public int[] Array { get; private set; }
+        public int Top { get; private set; }
+        public int Max { get; private set; }
+
+        public void Push(int val)
+        {
+            Top += 1;
+
+            if (Top >= Max)
+            {
+                throw new StackOverflowException();
+            }
+
+            Array[Top] = val;
+        }
+
+        public int Pop()
+        {
+            if (Top < 0)
+            {
+                Console.WriteLine("Stack underflow");
+                return default;
+            }
+
+            int val = Array[Top];
+            Array[Top] = default;
+            Top -= 1;
+            return val;
+        }
+        
+        public int GetVal(int v)
+        {
+            return Array[v];
+        }
+    }
+}
---
+++ DataStructuresAlgorithms/DataStructuresAlgorithmsTests/DataStructures/StackTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using DataStructuresAlgorithms.DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuresAlgorithmsTests.DataStructures
+{
+    [TestClass]
+    public class StackTests
+
+    {
+        [TestMethod]
+        public void CanPushValidValueToStack()
+        {
+            Stack stack = new Stack(5);
+            stack.Push(5);
+
+            Assert.AreEqual(0, stack.Top);
+            Assert.AreEqual(5, stack.GetVal(0));
+        }
+
+        [TestMethod]
+        public void CannotExceedStackSize()
+        {
+            Stack stack = new Stack(2);
+            stack.Push(5);
+            stack.Push(5);
+
+            Assert.AreEqual(1, stack.Top);
+            Assert.ThrowsException<StackOverflowException>(() => stack.Push(5));
+        }
+
+        [TestMethod]
+        public void PopReturnsExpectedValue()
+        {
+            Stack stack = new Stack(2);
+            stack.Push(2);
+            stack.Push(5);
+
+            Assert.AreEqual(5, stack.Pop());
+            Assert.AreEqual(0, stack.GetVal(1));
+            Assert.AreEqual(0, stack.Top);
+        }
+
+        [TestMethod]
+        public void CannotPopEmptyStack()
+        {
+            Stack stack = new Stack(2);
+
+            Assert.AreEqual(0, stack.Pop());
+            Assert.AreEqual(-1, stack.Top);
+        }
+    }
+}

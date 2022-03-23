8fc394e267ac80964c0657aff15993d10804724e
JosephEid <josephceid@gmail.com>
2022-03-23 18:53:54 +0000

    Implemented Queue

---
+++ DataStructuresAlgorithms/DataStructuresAlgorithms/DataStructures/Queue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace DataStructuresAlgorithms.DataStructures
+{
+    public class Queue
+    {
+        public Queue(int size)
+        {
+            Array = new int[size];
+            Front = -1;
+            Back = -1;
+            Max = size;
+        }
+
+        public int[] Array { get; private set; }
+        public int Front { get; private set; }
+        public int Back { get; private set; }
+        public int Max { get; private set; }
+
+        public void Enqueue(int val)
+        {
+            if (Back + 1 == Max)
+            {
+                Console.WriteLine("queue full");
+                return;
+            }
+
+            Back++;
+
+            if (Front == -1) Front++;
+
+            Array[Back] = val;
+        }
+
+        public int Dequeue()
+        {
+            if (Front > Back || Back == -1)
+            {
+                Console.WriteLine("Queue Empty");
+                Front = -1;
+                Back = -1;
+            }
+            if (Front >= 0)
+            {
+                int val = Array[Front];
+                Array[Front] = default;
+                Front++;
+
+                return val;
+            }
+
+            return 0;
+        }
+
+        public int GetVal(int v)
+        {
+            return Array[v];
+        }
+    }
+}
---
+++ DataStructuresAlgorithms/DataStructuresAlgorithmsTests/DataStructures/QueueTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using DataStructuresAlgorithms.DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuresAlgorithmsTests.DataStructures
+{
+    [TestClass]
+    public class QueueTests
+
+    {
+        [TestMethod]
+        public void CanQueueValidValue()
+        {
+            Queue queue = new Queue(5);
+
+            Assert.AreEqual(-1, queue.Front);
+            Assert.AreEqual(-1, queue.Back);
+
+            queue.Enqueue(5);
+
+            Assert.AreEqual(0, queue.Front);
+            Assert.AreEqual(0, queue.Back);
+
+            Assert.AreEqual(5, queue.GetVal(0));
+        }
+
+        [TestMethod]
+        public void CannotExceedQueueSize()
+        {
+            Queue queue = new Queue(2);
+
+            Assert.AreEqual(-1, queue.Front);
+            Assert.AreEqual(-1, queue.Back);
+
+            queue.Enqueue(5);
+            queue.Enqueue(5);
+
+            Assert.AreEqual(0, queue.Front);
+            Assert.AreEqual(1, queue.Back);
+
+            queue.Enqueue(5);
+            Assert.AreEqual(1, queue.Back); //queue back should still be 1
+        }
+
+        [TestMethod]
+        public void DequeueReturnsExpectedValue()
+        {
+            Queue queue = new Queue(2);
+
+            queue.Enqueue(2);
+            queue.Enqueue(5);
+
+            Assert.AreEqual(2, queue.Dequeue());
+            Assert.AreEqual(0, queue.GetVal(0));
+            Assert.AreEqual(1, queue.Front);
+        }
+
+        [TestMethod]
+        public void CannotDequeueEmptyQueue()
+        {
+            Queue queue = new Queue(5);
+
+            queue.Enqueue(3);
+            queue.Enqueue(5);
+            queue.Dequeue();
+            queue.Dequeue();
+            Assert.AreEqual(0, queue.Dequeue());
+
+            Assert.AreEqual(-1, queue.Front);
+            Assert.AreEqual(-1, queue.Back);
+        }
+    }
+}
